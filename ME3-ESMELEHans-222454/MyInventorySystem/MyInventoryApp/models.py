a9ed8b3d9fbc468f32f5ec3cdc19d6a766414c86
RG-55 <johannesesmele@gmail.com>
2024-02-20 16:19:38 +0800

    Added values
    Added 3 more items and 1 supplier

--- ME3-ESMELEHans-222454/MyInventorySystem/MyInventoryApp/models.py
+++ ME3-ESMELEHans-222454/MyInventorySystem/MyInventoryApp/models.py
@@ -18,17 +18,17 @@ class Supplier(models.Model):
 class WaterBottle(models.Model):
     sku = models.CharField(max_length=300, default='default')
     brand = models.CharField(max_length=300, default='default')
-    cost = models.CharField(max_length=300, default='default')
+    cost = models.DecimalField(max_digits=20,decimal_places=2, default=1)
     size = models.CharField(max_length=300, default='default')
     mouthsize = models.CharField(max_length=300, default='default')
     color = models.CharField(max_length=300, default='default')
-    suppliedby = models.ForeignKey(Supplier, on_delete=models.CASCADE,)
-    currentquant = models.CharField(max_length=300, default='default')
+    suppliedby = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    currentquant = models.IntegerField(default=1)
     
     def __str__(self):
         strreturn = "{}: {}, {}, {}, {}, supplied by {}, PHP {}: {}".format(self.sku,self.brand,self.mouthsize, self.size, self.color,self.suppliedby,self.cost,self.currentquant)
         return strreturn
-# Create your models here. models.ForeignKey(Supplier, on_delete=models.CASCADE,null=True)
+# Create your models here.
     
 #CHECK FOLLOWING (Not yet Implemented directly)
 #Ident of WB by SKU
